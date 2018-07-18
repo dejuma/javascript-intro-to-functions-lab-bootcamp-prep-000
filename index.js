1073f9fb39ba6818df799537a92015f4bfa0b333
Oluwadunsin Joseph Olesin <josepholesin@hotmail.com>
2018-07-18 18:54:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var lowercase = 'hello'
-  if (string.toLowerCase() === lowercase) {
-      return "I can\'t hear you!"
-} else if (string === "I love you, Grandma.") {
-      return "I love you, too."
-}
-}
+  if (string === string.toLowerCase()) {
+    return "I can\'t hear you!"
+  }
+
+  function sayHiToGrandma(string) {
+    if (string === string.toUpperCase()) {
+      return "YES INDEED!"
+    }
